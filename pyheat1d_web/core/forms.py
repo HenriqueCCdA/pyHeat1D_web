5ec1aac41c626f5609e6bc91bf717acb71fe0113
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-01-03 01:03:48 -0300

    [REFAC: #3] - Add o bootstrap.
    close #3

--- pyheat1d_web/core/forms.py
+++ pyheat1d_web/core/forms.py
@@ -6,14 +6,43 @@ BC_TYPES = {1: "Temperatura", 3: "Newton"}
 
 
 class NewAnalysisForm(forms.ModelForm):
-    length = forms.FloatField(label="Comprimento", initial=1.0)
-    ndiv = forms.IntegerField(label="Numero de divisões", initial=10_000)
-    dt = forms.FloatField(label="Passo de Tempo", initial=1.0)
-    nstep = forms.IntegerField(label="Número de Passos", initial=100)
-    initialt = forms.FloatField(label="Temperatura Inicial", initial=1.0)
-    prop_k = forms.FloatField(label="Coeficiente de difusão", initial=1.0)
-    prop_ro = forms.FloatField(label="Calor especifico", initial=1.0)
-    prop_cp = forms.FloatField(label="Criando um analise", initial=1.0)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs["class"] = "form-control col-sm-10"
+
+    length = forms.FloatField(
+        label="Comprimento",
+        initial=1.0,
+    )
+    ndiv = forms.IntegerField(
+        label="Numero de divisões",
+        initial=10_000,
+    )
+    dt = forms.FloatField(
+        label="Passo de Tempo",
+        initial=1.0,
+    )
+    nstep = forms.IntegerField(
+        label="Número de Passos",
+        initial=100,
+    )
+    initialt = forms.FloatField(
+        label="Temperatura Inicial",
+        initial=1.0,
+    )
+    prop_k = forms.FloatField(
+        label="Coeficiente de difusão",
+        initial=1.0,
+    )
+    prop_ro = forms.FloatField(
+        label="Calor especifico",
+        initial=1.0,
+    )
+    prop_cp = forms.FloatField(
+        label="Criando um analise",
+        initial=1.0,
+    )
 
     lbc_type = forms.ChoiceField(
         label="Tipo condição de contorno a Esquerda",

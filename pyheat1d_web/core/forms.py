3ffa9d515d4a0579bd1fde7d617c25bc5a62c3f0
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-01-05 00:51:12 -0300

    [FEAT: #8] - Criar a opção de editar simulação.
    close #8

--- pyheat1d_web/core/forms.py
+++ pyheat1d_web/core/forms.py
@@ -5,67 +5,37 @@ from .models import Simulation
 BC_TYPES = {1: "Temperatura", 3: "Newton"}
 
 
-class NewAnalysisForm(forms.ModelForm):
+class NewSimulationForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for visible in self.visible_fields():
             visible.field.widget.attrs["class"] = "form-control col-sm-10"
 
-    length = forms.FloatField(
-        label="Comprimento",
-        initial=1.0,
-    )
-    ndiv = forms.IntegerField(
-        label="Numero de divisões",
-        initial=100_000,
-    )
-    dt = forms.FloatField(
-        label="Passo de Tempo",
-        initial=0.0001,
-    )
-    nstep = forms.IntegerField(
-        label="Número de Passos",
-        initial=10_000,
-    )
-    initialt = forms.FloatField(
-        label="Temperatura Inicial",
-        initial=100.0,
-    )
-    # prop_k = forms.FloatField(
-    #     label="Coeficiente de difusão",
-    #     initial=1.0,
-    # )
-    # prop_ro = forms.FloatField(
-    #     label="Calor especifico",
-    #     initial=1.0,
-    # )
-    # prop_cp = forms.FloatField(
-    #     label="Criando um analise",
-    #     initial=1.0,
-    # )
+    length = forms.FloatField(label="Comprimento", initial=1.0)
+    ndiv = forms.IntegerField(label="Numero de divisões", initial=100_000)
+    dt = forms.FloatField(label="Passo de Tempo", initial=0.0001)
+    nstep = forms.IntegerField(label="Número de Passos", initial=10_000)
+    initialt = forms.FloatField(label="Temperatura Inicial", initial=100.0)
 
-    # lbc_type = forms.ChoiceField(
-    #     label="Tipo condição de contorno a Esquerda",
-    #     required=False,
-    #     choices=BC_TYPES,
-    # )
+    lbc_value = forms.FloatField(label="Temperatura a esquerda", initial=0.0)
 
-    lbc_value = forms.FloatField(
-        label="Temperatura a esquerda",
-        initial=0.0,
-    )
-
-    # rbc_type = forms.ChoiceField(
-    #     label="Tipo condição de contorno a Direita",
-    #     required=False,
-    #     choices=BC_TYPES,
-    # )
-
-    rbc_value = forms.FloatField(
-        label="Temperatura a direita",
-        initial=0.0,
-    )
+    rbc_value = forms.FloatField(label="Temperatura a direita", initial=0.0)
 
     class Meta:
         model = Simulation
         fields = ("tag",)
+
+
+class EditSimulationForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs["class"] = "form-control col-sm-10"
+
+    length = forms.FloatField(label="Comprimento")
+    ndiv = forms.IntegerField(label="Numero de divisões")
+    dt = forms.FloatField(label="Passo de Tempo")
+    nstep = forms.IntegerField(label="Número de Passos")
+    initialt = forms.FloatField(label="Temperatura Inicial")
+    lbc_value = forms.FloatField(label="Temperatura a esquerda")
+    rbc_value = forms.FloatField(label="Temperatura a direita")
--- pyheat1d_web/core/urls.py
+++ pyheat1d_web/core/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("", views.analysis_list, name="analysis_list"),
     path("create/", views.create_analysis_form, name="create_analysis_form"),
     path("run/<int:pk>/", views.run_analysis, name="run_analysis"),
+    path("edit/<int:pk>/", views.edit_analysis_form, name="edit_analysis_form"),
     path("detail/<int:pk>/", views.analysis_detail, name="analysis_detail"),
     path("delete/<int:pk>/", views.analysis_delete, name="analysis_delete"),
     path("results/<int:pk>/", views.simulation_results, name="simulation_results"),

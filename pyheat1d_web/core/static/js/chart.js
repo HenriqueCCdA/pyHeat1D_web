fe49e56700f42af87a679bf8bce05a8c2508a8e5
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-01-07 03:22:06 -0300

    [REFAC: #35] - Reorganizando do html.
    close #35

---
+++ pyheat1d_web/core/static/js/chart.js
@@ -0,0 +1,50 @@
+function plot(data) {
+
+  const ctx = document.getElementById('plot_area');
+  const configs = {
+    elements: {
+      point:{
+        radius: 0
+      }
+    },
+    plugins: {
+      title: {
+        display: true,
+        text: 'Temperaturas ao longo de X'
+      },
+    },
+    scales: {
+      x: {
+        display: true,
+        title: {
+          display: true,
+          text: 'x(m)'
+        }
+      },
+      y: {
+        display: true,
+        title: {
+          display: true,
+          text: '°C'
+        },
+        beginAtZero: true
+      },
+    }
+  }
+
+  const graph_data = {
+      labels: data.mesh,
+      datasets: data.steps.map(element => {
+          return {
+            label: `Tempo ${element.t} s`,
+            data: element.u,
+            borderWidth: 1
+          }
+      })
+    }
+  new Chart(ctx, {
+    type: 'line',
+    data: graph_data,
+    options:configs,
+  });
+}
